0a693687df48403113f5fabb0e51541745d1766b
chiti62 <r06922129@csie.ntu.edu.tw>
2020-02-25 10:37:27 +0100

    'ws test'

--- lab2/flasktest.py
+++ lab2/flasktest.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route("/")
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+
+# if __name__ == "__main__":
+#     app.run()
+from flask import Flask
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        ws.send(message)
+
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
+
 
 if __name__ == "__main__":
     app.run()
