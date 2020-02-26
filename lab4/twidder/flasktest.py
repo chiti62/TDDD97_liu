750b5696dd1084e0adfc57084b73c1e67394bf83
chiti62 <r06922129@csie.ntu.edu.tw>
2020-02-26 22:56:58 +0100

    test

---
+++ lab4/twidder/flasktest.py
@@ -0,0 +1,32 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
+# from flask import Flask
+# from flask_sockets import Sockets
+
+# app = Flask(__name__)
+# sockets = Sockets(app)
+
+
+# @sockets.route('/echo')
+# def echo_socket(ws):
+#     while True:
+#         message = ws.receive()
+#         ws.send(message)
+
+
+# @app.route('/')
+# def hello():
+#     return 'Hello World!'
+
+
+# if __name__ == "__main__":
+#     app.run()
